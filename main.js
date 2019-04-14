be2eebfa7ffc28c8403cf8d6eac8e6e558c21fa3
AnastasiiaDidenko <a.94didenko@gmail.com>
2019-04-14 17:09:38 +0300

    I changed the class names in the burger menu

--- main.js
+++ main.js
@@ -3,15 +3,15 @@ $(document).ready(function(){
 		offset: 50
 		});// Плавный скрол привязвн к ссылкам
 		
-		$('.menu__icon').on('click', function() {
-			$(this).closest('.menu')
-				.toggleClass('menu_state_open');
+		$('.main-nav__icon').on('click', function() {
+			$(this).closest('.main-nav')
+				.toggleClass('main-nav_state_open');
 		});
 		
 		$('.main-nav__link').on('click', function() {
 			// do something
 
-			$(this).closest('.menu')
-				.removeClass('menu_state_open');
+			$(this).closest('.main-nav')
+				.removeClass('main-nav_state_open');
 		});
 });
