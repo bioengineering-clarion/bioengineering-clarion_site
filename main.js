2f69bab4c02e86be70b102475e36ca867a1167d9
AnastasiiaDidenko <a.94didenko@gmail.com>
2019-04-14 14:58:47 +0300

    I created a burger menu for mobile

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
 	$('.main-nav .main-nav__link').mPageScroll2id({
 		offset: 50
-	  });// Плавный скрол привязвн к ссылкам
+		});// Плавный скрол привязвн к ссылкам
+		
+		$('.menu__icon').on('click', function() {
+			$(this).closest('.menu')
+				.toggleClass('menu_state_open');
+		});
+		
+		$('.main-nav__link').on('click', function() {
+			// do something
+
+			$(this).closest('.menu')
+				.removeClass('menu_state_open');
+		});
 });
